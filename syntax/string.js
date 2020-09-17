019c5d95e0d066b2ca21feac6b330a2aebb9d166
bigcat0815 <bigcat0815@gmail.com>
2020-09-18 03:35:13 +0900

    #url

--- syntax/string.js
+++ syntax/string.js
@@ -1,2 +1,13 @@
 var name = 'Lorem ipsum...s'.length;
 console.log(name);
+
+var letter = `Dear ${name}`;
+
+console.log(letter);
+
+var myName = 'Master';
+var letter2 = `Dear ${myName} 
+
+Thankyou ${myName}`;
+
+console.log(letter2);
