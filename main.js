330e1eb989ab312b63c761ad3c38ff0477e85e85
bigcat0815 <bigcat0815@gmail.com>
2020-09-27 08:56:02 +0900

    #19-2 . Homepage

--- main.js
+++ main.js
@@ -5,35 +5,67 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;    
     var queryData = url.parse(_url,true).query;
-    var pathname = url.parse(_url,true).pathname;
-    var title = queryData.id;
+    var pathname = url.parse(_url,true).pathname;   
     
     if(pathname ==='/')
     {
-      fs.readFile(`data/${title}`,'utf8',function(err,descripction){
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ol>
-          <h2>${title}</h2>
-          <p>${descripction}</p>
-        </body>
-        </html>
-        `;
-        response.writeHead(200);
-        response.end(template);
-      })
-    }
+      if(  queryData.id === undefined)
+      {
+        fs.readFile(`data/${ queryData.id}`,'utf8',function(err,descripction){
+          title = 'Welcome';
+          descripction = 'Hello, Node.js';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${descripction}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        })
+      } 
+      else
+      {
+        fs.readFile(`data/${queryData.id}`,'utf8',function(err,descripction){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${descripction}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        })
+      }
+      }
+      
     else
     {
       response.writeHead(404);
